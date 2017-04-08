6c19d0a7f6ea2283bf447c97a0716cdb509c047a
GraemeB <grae22@gmail.com>
2017-04-08 13:32:49 +0200

    Adding Vidly project.

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using System.Collections.Generic;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+  public class MoviesController : Controller
+  {
+    // GET: Movies
+    public ViewResult Random()
+    {
+      var movie = new Movie() { Name = "Shrek!" };
+
+      var customers = new List<Customer>
+      {
+        new Customer() { Name = "Customer 1" },
+        new Customer() { Name = "Customer 2" }
+      };
+
+      var viewModel = new RandomMovieViewModel
+      {
+        Movie = movie,
+        Customers = customers
+
+      };
+
+      return View( viewModel );
+    }
+
+    [Route( "movies/released/{year}/{month:regex(\\d{2}):range( 1, 12 )}" )]
+    public ActionResult ByReleaseDate( int year, int month )
+    {
+      return Content( year + "/" + month );
+    }
+  }
+}
