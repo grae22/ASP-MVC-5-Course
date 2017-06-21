fdb4ec08493ea7e3377394ea79d5577554a42b72
GraemeB <grae22@gmail.com>
2017-06-21 21:03:38 +0200

    Genre displaying in movies index.

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -1,18 +1,31 @@
 ﻿using System.Web.Mvc;
-using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 using Vidly.Models;
 
 namespace Vidly.Controllers
 {
   public class MoviesController : Controller
   {
+    ApplicationDbContext _context;
+
+    public MoviesController()
+    {
+      _context = new ApplicationDbContext();
+    }
+
+    protected override void Dispose( bool disposing )
+    {
+      _context.Dispose();
+    }
+
     public ViewResult Index()
     {
-      var movies = new List<Movie>()
-      {
-        new Movie() { Name = "Shrek" },
-        new Movie() { Name = "Wall-e" }
-      };
+      var movies =
+        _context
+          .Movies
+          .Include( m => m.Genre )
+          .ToList();
       
       return View( movies );
     }
--- Vidly/Vidly/Models/Movie.cs
+++ Vidly/Vidly/Models/Movie.cs
@@ -4,5 +4,6 @@
   {
     public int Id { get; set; }
     public string Name { get; set; }
+    public Genre Genre { get; set; }
   }
 }
