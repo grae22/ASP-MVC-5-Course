c7f62a239aa00414395d7bea9dcf6a2f5e14f99b
GraemeB <grae22@gmail.com>
2017-06-21 21:07:37 +0200

    Properties added to movie.

--- Vidly/Vidly/Models/Movie.cs
+++ Vidly/Vidly/Models/Movie.cs
@@ -1,9 +1,14 @@
-﻿namespace Vidly.Models
+﻿using System;
+
+namespace Vidly.Models
 {
   public class Movie
   {
     public int Id { get; set; }
     public string Name { get; set; }
     public Genre Genre { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public DateTime DateAdded { get; set; }
+    public byte NumberInStock { get; set; }
   }
 }
