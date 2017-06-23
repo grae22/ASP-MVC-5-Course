0337ddae7ba3ee16ec27ff7280d859d46d10f10d
GraemeB <grae22@gmail.com>
2017-06-23 19:33:39 +0200

    Prep for updating existing customer.

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -26,12 +26,12 @@ namespace Vidly.Controllers
     {
       var membershipTypes = _context.MembershipTypes.ToList();
 
-      var viewModel = new NewCustomerViewModel
+      var viewModel = new CustomerFormViewModel
       {
         MembershipTypes = membershipTypes,
       };
 
-      return View( viewModel );
+      return View( "CustomerForm", viewModel );
     }
 
     [HttpPost]
@@ -41,6 +41,24 @@ namespace Vidly.Controllers
       _context.SaveChanges();
 
       return RedirectToAction( "Index", "Customers" );
+    }
+
+    public ActionResult Edit( int id )
+    {
+      var customer = _context.Customers.SingleOrDefault( c => c.Id == id );
+
+      if( customer == null )
+      {
+        return HttpNotFound();
+      }
+
+      var viewModel = new CustomerFormViewModel
+      {
+        Customer = customer,
+        MembershipTypes = _context.MembershipTypes.ToList()
+      };
+
+      return View( "CustomerForm", viewModel );
     }
 
     public ViewResult Index()
