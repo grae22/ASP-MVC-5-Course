3a86bde67dafcbb53ed2b50617b9f282433a3192
GraemeB <grae22@gmail.com>
2017-07-16 15:30:34 +0200

    [WIP] API for handling rentals.

---
+++ Vidly/Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+  public class NewRentalsController : ApiController
+  {
+    ApplicationDbContext _context;
+
+    public NewRentalsController()
+    {
+      _context = new ApplicationDbContext();
+    }
+
+    // POST: /api/newRentals
+    [HttpPost]
+    public IHttpActionResult CreateNewRental( NewRentalDto newRental )
+    {
+      if( ModelState.IsValid == false )
+      {
+        return BadRequest();
+      }
+
+      Customer customer = _context.Customers.Single( c => c.Id == newRental.CustomerId );
+
+      foreach( int movieId in newRental.MovieIds )
+      {
+        Movie movie = _context.Movies.Single( m => m.Id == movieId );
+
+        var rental = new Rental
+        {
+          Customer = customer,
+          Movie = movie,
+          DateRented = DateTime.Now
+        };
+
+        _context.Rentals.Add( rental );
+      }
+
+      _context.SaveChanges();
+
+      return Ok();
+    }
+  }
+}
