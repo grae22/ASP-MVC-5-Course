be05a347fb3b95bc376251b74f4cc26a4f07fe74
GraemeB <grae22@gmail.com>
2017-08-31 07:15:10 +0200

    Filtering of typeahead results. Rental form submission.

--- Vidly/Vidly/Controllers/Api/MoviesController.cs
+++ Vidly/Vidly/Controllers/Api/MoviesController.cs
@@ -19,16 +19,27 @@ namespace Vidly.Controllers.Api
     }
 
     // GET: /api/movies
-    public IHttpActionResult GetMovies()
+    public IHttpActionResult GetMovies(string query)
     {
-      return Ok(
+      var moviesQuery =
         _context
           .Movies
-          .Include( m => m.Genre )
-          .ToList()
-          .Select( Mapper.Map< Movie, MovieDto > ) );
-    }
+          .Include(m => m.Genre)
+          .Where(m => m.NumberAvailable > 0);
 
+      if (!string.IsNullOrWhiteSpace(query))
+      {
+        moviesQuery = moviesQuery.Where(m => m.Name.Contains(query));
+      }
+
+      var movieDtos =
+        moviesQuery
+          .ToList()
+          .Select( Mapper.Map< Movie, MovieDto > );
+
+      return Ok(movieDtos);
+    }
+    
     // GET: /api/movies/1
     public IHttpActionResult GetMovie( int id )
     {
